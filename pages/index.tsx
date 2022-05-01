11174dbeabd893f82d63f091d0868407d6423aa7
Muzzamil Oreoluwa <ore@Oreoluwas-MacBook-Pro.local>
2022-05-01 13:57:49 +0100

    app += completed test

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
         </h1>
 
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <Link href="/products">
+          <Link href="/product">
             <a className="mt-6 w-96 rounded-xl border p-6 text-left text-center hover:text-blue-600 focus:text-blue-600">
               <h3 className="text-2xl font-bold">Product &rarr;</h3>
               <p className="mt-4 text-xl">Check the product page</p>
